5d1bfce56e65f4fab8238f8cdeb477c56ec5086c
Tony Jian <tonytonyjan@gmail.com>
2014-05-06 01:20:57 +0800

    prevent load faker.rb in production env

--- config/initializers/faker.rb
+++ config/initializers/faker.rb
@@ -1,9 +1,11 @@
-Faker::Config.locale = :'zh-TW'
+unless Rails.env.production?
+  Faker::Config.locale = :'zh-TW'
 
-class Faker::Image
-  class << self
-    def image
-      Rack::Test::UploadedFile.new(Dir[Rails.root.join('spec', 'fixtures', 'images', '*')].sample)
+  class Faker::Image
+    class << self
+      def image
+        Rack::Test::UploadedFile.new(Dir[Rails.root.join('spec', 'fixtures', 'images', '*')].sample)
+      end
     end
   end
 end
