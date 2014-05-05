5d8b64f677dc1868097700092886b88784e43212
Tony Jian <tonytonyjan@gmail.com>
2014-05-06 00:41:15 +0800

    integrate sidebar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,17 @@
 class PostsController < ApplicationController
+  before_action :set_recent_posts
+
   def index
     @posts = Post.order('id DESC').page(params[:page]).per(3)
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  private
+
+  def set_recent_posts
+    @recent_posts = Post.order('id DESC').limit(3)
   end
 end
