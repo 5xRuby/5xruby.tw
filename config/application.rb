497eea23b2844c7143ad8a87a941e9e81e670990
Tony Jian <tonytonyjan@gmail.com>
2014-05-05 09:12:28 +0800

    config

--- config/application.rb
+++ config/application.rb
@@ -21,10 +21,14 @@ module PentaRuby
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Taipei'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :'zh-TW'
+
+    config.generators.assets = false
+    config.generators.helper = false
+    config.generators.jbuilder = false
   end
 end
