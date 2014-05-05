13707de6cda7c3819a3d6f60f19cb6f4bfdf2d1b
Tony Jian <tonytonyjan@gmail.com>
2014-05-05 09:14:55 +0800

    add index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
