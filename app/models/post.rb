a16cdd5caf7ad832d0a76f61a098e98643e0ebba
Tony Jian <tonytonyjan@gmail.com>
2014-05-05 13:13:33 +0800

    use model template

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+  # scope macros
+
+  # Concerns macros
+
+  # Constants
+  
+  # Attributes related macros
+
+  # association macros
+
+  # validation macros
+
+  # callbacks
+
+  # other
+
+  protected
+  # callback methods
 end
