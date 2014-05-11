2b0adb4bc7fd353bcfa8201937ce58110f7498c7
Tony Jian <tonytonyjan@gmail.com>
2014-05-12 01:17:10 +0800

    beatify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,23 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :applies
-  end
-
+  # front
   root 'pages#index'
   controller :pages do
     get :about, :contacts
   end
 
-  get 'select2/tags', format: :json
-  get 'select2/results', format: :json
-
   resources :posts, :courses, only: %i[index show]
-  resources :redactor_images, :contacts, only: :create
+  resources :applies, only: %i[new create create] do
+    get :course, :message, :finish, on: :member
+  end
 
+  # back
   namespace :admin do
     root to: :dashboard
-    resources :posts, :courses, :schedules
+    resources :posts, :courses, :schedules, :applies
   end
+
+  # plugins
+  get 'select2/tags', format: :json
+  get 'select2/results', format: :json
+  resources :redactor_images, :contacts, only: :create
 end
