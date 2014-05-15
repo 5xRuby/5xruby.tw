28d36047aacc787a1f611a924c020477766911ba
Tony Jian <tonytonyjan@gmail.com>
2014-05-16 02:11:46 +0800

    update speakers schema

--- app/models/admin/speaker.rb
+++ app/models/admin/speaker.rb
@@ -2,13 +2,19 @@
 #
 # Table name: speakers
 #
-#  id         :integer          not null, primary key
-#  avatar     :string(255)
-#  name       :string(255)      not null
-#  summary    :text
-#  is_online  :boolean          default(FALSE), not null
-#  created_at :datetime
-#  updated_at :datetime
+#  id           :integer          not null, primary key
+#  avatar       :string(255)
+#  name         :string(255)      not null
+#  summary      :text
+#  is_online    :boolean          default(FALSE), not null
+#  created_at   :datetime
+#  updated_at   :datetime
+#  role         :string(255)      default("consultant"), not null
+#  title        :string(255)
+#  facebook_url :string(255)
+#  twitter_url  :string(255)
+#  google_url   :string(255)
+#  blog_url     :string(255)
 #
 
 class Admin::Speaker < Speaker
--- spec/factories/speakers.rb
+++ spec/factories/speakers.rb
@@ -2,13 +2,19 @@
 #
 # Table name: speakers
 #
-#  id         :integer          not null, primary key
-#  avatar     :string(255)
-#  name       :string(255)      not null
-#  summary    :text
-#  is_online  :boolean          default(FALSE), not null
-#  created_at :datetime
-#  updated_at :datetime
+#  id           :integer          not null, primary key
+#  avatar       :string(255)
+#  name         :string(255)      not null
+#  summary      :text
+#  is_online    :boolean          default(FALSE), not null
+#  created_at   :datetime
+#  updated_at   :datetime
+#  role         :string(255)      default("consultant"), not null
+#  title        :string(255)
+#  facebook_url :string(255)
+#  twitter_url  :string(255)
+#  google_url   :string(255)
+#  blog_url     :string(255)
 #
 
 # Read about factories at https://github.com/thoughtbot/factory_girl
@@ -19,5 +25,11 @@ FactoryGirl.define do
     name { Faker::Name.name }
     summary { Faker::Lorem.paragraph }
     is_online { [true, false].sample }
+    role { Speaker.role.values.sample }
+    title { Faker::Name.title }
+    facebook_url { Faker::Internet.url('facebook.com') }
+    twitter_url { Faker::Internet.url('twitter.com') }
+    google_url { Faker::Internet.url('google.com', "/+#{Faker::Internet.user_name}") }
+    blog_url { Faker::Internet.url }
   end
 end
