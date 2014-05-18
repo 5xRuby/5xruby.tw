def1aa177336d88c7c3fa50e1dfcc009dbd61d53
Tony Jian <tonytonyjan@gmail.com>
2014-05-18 20:34:50 +0800

    integerate tags and posts view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,10 @@
 class PostsController < ApplicationController
-  before_action :set_recent_posts
+  before_action :set_recent_posts, :set_tags
 
   def index
     @posts = Post.order('id DESC').page(params[:page]).per(3)
+    @query_tags = params[:tags].is_a?(String) ? params[:tags].split(',') : params[:tags]
+    @posts = @posts.tagged_with(@query_tags) if @query_tags.present?
   end
 
   def show
@@ -13,5 +15,9 @@ class PostsController < ApplicationController
 
   def set_recent_posts
     @recent_posts = Post.order('id DESC').limit(3)
+  end
+
+  def set_tags
+    @tags = Post.tags_on(:tags)
   end
 end
