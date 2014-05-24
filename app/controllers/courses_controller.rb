ef00cf136f8129dbd8c17c75cc21a0bbf2b22a17
Eddie Kao <eddie@digik.com.tw>
2014-05-24 22:21:33 +0800

    check category exists

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,7 +2,7 @@ class CoursesController < ApplicationController
   def index
     @courses = Course.online.order('id DESC').page(params[:page]).per(6)
     @courses = @courses.where(category: @category) if @category = Category.find_by(id: params[:category_id])
-    @categories = Category.order(:sort_id).where('courses_count > ?', 0)
+    @categories = Category.order(:sort_id).where('courses_count > ?', 0) if @courses.presence
   end
 
   def show
