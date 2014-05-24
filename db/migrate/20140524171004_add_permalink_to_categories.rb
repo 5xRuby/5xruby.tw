0bfa69bf2c7f6ca9a56de17a73bdeec00478e918
Tony Jian <tonytonyjan@gmail.com>
2014-05-25 02:21:55 +0800

    fix migration

--- db/migrate/20140524171004_add_permalink_to_categories.rb
+++ db/migrate/20140524171004_add_permalink_to_categories.rb
@@ -1,6 +1,6 @@
 class AddPermalinkToCategories < ActiveRecord::Migration
   def change
-    add_column :categories, :permalink, :string, null: false, default: SecureRandom.hex(4)
+    add_column :categories, :permalink, :string, null: false
     add_index :categories, :permalink, unique: true
   end
 end
--- db/migrate/20140524171005_add_permalink_to_courses.rb
+++ db/migrate/20140524171005_add_permalink_to_courses.rb
@@ -1,6 +1,6 @@
 class AddPermalinkToCourses < ActiveRecord::Migration
   def change
-    add_column :courses, :permalink, :string, null: false, default: SecureRandom.hex(4)
+    add_column :courses, :permalink, :string, null: false
     add_index :courses, :permalink, unique: true
   end
 end
