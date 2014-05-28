55a1a8f6263548055319689dd9bfc32e09391abd
Tony Jian <tonytonyjan@gmail.com>
2014-05-28 18:08:32 +0800

    rename helper #apply_link

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,9 +1,11 @@
 module CoursesHelper
   # [obj] can be an instance of Course or Schedule
-  def apply_link obj
-    case obj
-    when Course then obj.apply_link.presence || new_course_apply_path(obj)
-    when Schedule then obj.stage.course.apply_link.presence || new_course_apply_path(obj.stage.course, apply: {stage_id: obj.stage.id, schedule_id: obj.id})
+  def apply_url obj, **options
+    if course = case obj
+      when Course then obj
+      when Schedule then obj.stage.course
+      end
+      course.apply_link.presence || new_course_apply_path(course, **options)
     end
   end
 end
