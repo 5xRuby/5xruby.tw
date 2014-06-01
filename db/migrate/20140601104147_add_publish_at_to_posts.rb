40dcdbeb5fdfe5fe627b4334ed04c37d4e8f7037
Tony Jian <tonytonyjan@gmail.com>
2014-06-02 03:28:51 +0800

    add default value of publish_at

--- db/migrate/20140601104147_add_publish_at_to_posts.rb
+++ db/migrate/20140601104147_add_publish_at_to_posts.rb
@@ -1,5 +1,6 @@
 class AddPublishAtToPosts < ActiveRecord::Migration
   def change
     add_column :posts, :publish_at, :datetime
+    Post.find_each{ |post| post.update publish_at: post.created_at }
   end
 end
