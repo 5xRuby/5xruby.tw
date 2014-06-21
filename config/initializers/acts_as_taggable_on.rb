19633d89d4d03f593c180b1657d6e2b5ee0794d1
Tony Jian <tonytonyjan@gmail.com>
2014-06-21 17:36:13 +0800

    config acts_as_taggable_on:

---
+++ config/initializers/acts_as_taggable_on.rb
@@ -0,0 +1,14 @@
+# To change the tag list delimiter
+# ActsAsTaggableOn.delimiter = ' '
+
+# If you would like to remove unused tag objects after removing taggings:
+ActsAsTaggableOn.remove_unused_tags = true
+
+# If you want force tags to be saved downcased:
+#ActsAsTaggableOn.force_lowercase = true
+
+# If you want tags to be saved parametrized (you can redefine to_param as well):
+#ActsAsTaggableOn.force_parameterize = true
+
+# If you want tags to be matched case-sensitively:
+#ActsAsTaggableOn.strict_case_match = true
