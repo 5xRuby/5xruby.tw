fbb34a752aa2613c04837cfdfe61f83da801e1e3
Tony Jian <tonytonyjan@gmail.com>
2014-07-25 11:18:28 +0800

    rm useless code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,6 @@
 class PagesController < ApplicationController
   def index
     @next_schedules = Schedule.includes(stage: :course).available.where(stages: {sort_id: 0}).limit(4)
-    @feature_course = Course.find_by(permalink: 'ruby-on-rails')
   end
 
   def contacts
