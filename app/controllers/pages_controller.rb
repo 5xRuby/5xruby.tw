4136ea000063e26274a6a0478720f2e09c73cec0
Eddie Kao <eddie@digik.com.tw>
2014-08-21 08:58:18 +0800

    hide offline course(fixed)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @next_schedules = Schedule.includes(stage: :course).available.where(courses: {is_online: true}).where(stages: {sort_id: 1110}).limit(4)
+    @next_schedules = Schedule.includes(stage: :course).available.where(courses: {is_online: true}).where(stages: {sort_id: 0}).limit(4)
   end
 
   def contacts
