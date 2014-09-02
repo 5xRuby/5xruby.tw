c66a5c71baa99c1238cba6b337b6fdf6855c2c52
Tony Jian <tonytonyjan@gmail.com>
2014-09-02 22:43:58 +0800

    build association

--- app/models/course.rb
+++ app/models/course.rb
@@ -38,6 +38,7 @@ class Course < ActiveRecord::Base
   accepts_nested_attributes_for :stages, allow_destroy: true, reject_if: proc { |attributes| attributes[:title].blank? }
   has_many :schedules, through: :stages
   has_many :applies
+  has_and_belongs_to_many :speakers
   belongs_to :category, counter_cache: true
 
   # validation macros
