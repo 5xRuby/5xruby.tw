abd3ff479c70fed4fd635a5d5e936af28432d1d5
Tony Jian <tonytonyjan@gmail.com>
2014-09-12 16:01:07 +0800

    set up i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :set_seo
+  before_action :set_seo, :set_locale
 
   def recaptcha?
     return @recaptcha if @recaptcha
@@ -38,5 +38,14 @@ class ApplicationController < ActionController::Base
         image: 'og_picture.png'
       }
     }
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  # override
+  def default_url_options(options={})
+    {locale: I18n.locale}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  # front
-  root 'pages#index'
-  get :about, :contacts, :faq, :sitemap, controller: :pages
-
-  resources :posts, only: %i[index show]
-  resources :courses, path: :talks, only: %i[index show]
-  get 'courses/:id', to: redirect('/talks/%{id}')
-  get 'courses', to: redirect('/talks')
-  resources :speakers, only: :index
+  scope '(:locale)' do
+    root 'pages#index'
+    get :about, :contacts, :faq, :sitemap, controller: :pages
+    resources :posts, only: %i[index show]
+    resources :courses, path: :talks, only: %i[index show]
+    get 'courses/:id', to: redirect('/talks/%{id}')
+    get 'courses', to: redirect('/talks')
+    resources :speakers, only: :index
+  end
 
   #concern
   concern :sortable do
