34f586cb6c1b7851ac2a5f389a13cfaf9d68d742
Tony Jian <tonytonyjan@gmail.com>
2014-10-08 12:04:09 +0800

    annotate

--- spec/factories/showcases.rb
+++ spec/factories/showcases.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: showcases
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  description :text
+#  link        :string(255)
+#  image       :string(255)
+#  is_online   :boolean          default(FALSE), not null
+#  sort_id     :integer          default(0), not null
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
