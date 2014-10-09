d0a30129ca7688ec9984b82a13562a6ca71ad689
Tony Jian <tonytonyjan@gmail.com>
2014-10-10 02:49:03 +0800

    SEO for video page

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -7,5 +7,22 @@ class VideosController < ApplicationController
     @video = Video.find(params[:id])
     @next = @video.next
     @previous = @video.previous
+    @seo = {
+      meta: {
+        description: @video.summary
+      },
+      google: {
+        name: @video.title,
+        description: @video.summary,
+        embedUrl: @video.link,
+        item_type: :VideoObject
+      },
+      og: {
+        title: @video.title,
+        url: video_url(@video),
+        type: :'video.episode',
+        description: @video.summary
+      }
+    }
   end
 end
