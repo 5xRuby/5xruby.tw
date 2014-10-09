cb1824dae0489a144890d7b2d20159873cec595a
Tony Jian <tonytonyjan@gmail.com>
2014-10-10 02:56:32 +0800

    enhance admin video pages

--- app/helpers/admin/courses_helper.rb
+++ app/helpers/admin/courses_helper.rb
@@ -1,9 +1,9 @@
 module Admin::CoursesHelper
-  def tr_speakers course
+  def tr_speakers record
     [
       :speakers,
-      ->(course){
-        course.speakers.map{ |speaker| link_to speaker.name, speaker }.join(', ').html_safe
+      ->(record){
+        record.speakers.map{ |speaker| link_to speaker.name, speaker }.join(', ').html_safe
       }
     ]
   end
--- app/models/admin/video.rb
+++ app/models/admin/video.rb
@@ -22,6 +22,7 @@ class Admin::Video < Video
   # Attributes related macros
 
   # association macros
+  has_and_belongs_to_many :speakers
 
   # validation macros
 
