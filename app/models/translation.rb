bdcb0736fb3bac24493b5ce303df4342802a0faf
Tony Jian <tonytonyjan@gmail.com>
2014-10-23 18:03:13 +0800

    reorder

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,9 +1,9 @@
 class Translation < ActiveRecord::Base
   AVAILABLE_MODELS = {
-    Post => [:title, :summary, content: :redactor],
-    Course => [:title, :summary, :subtitle, what_will_learn: :redactor, description: :redactor, note: :redactor],
     Faq => [:question, :answer],
-    Speaker => [:name, :summary, :title]
+    Speaker => [:name, :summary],
+    Post => [:title, :summary, content: :redactor],
+    Course => [:title, :summary, :subtitle, what_will_learn: :redactor, description: :redactor, note: :redactor]
   }
   # scope macros
 
