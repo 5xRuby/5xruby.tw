c9f6f0b0d04bd07b5a33f00e8080270914d0bf7c
Tony Jian <tonytonyjan@gmail.com>
2014-10-23 17:55:15 +0800

    show origin text in input fields, and make them disabled

--- app/helpers/admin/translations_helper.rb
+++ app/helpers/admin/translations_helper.rb
@@ -2,7 +2,6 @@ module Admin::TranslationsHelper
   def admin_translations_available_columns record
     available_models = Translation::AVAILABLE_MODELS[record.class]
     available_names = []
-
     available_models.each do |value|
       value.is_a?(Hash) ? available_names += value.keys : available_names << value
     end
@@ -15,12 +14,26 @@ module Admin::TranslationsHelper
     end
   end
 
+  def admin_translations_disabled_field record, column
+    value = record.send(column.name)
+    available_models = Translation::AVAILABLE_MODELS[record.class]
+    hash = available_models.last.is_a?(Hash) ? available_models.last : {}
+    case hash[column.name.to_sym]
+    when :redactor then text_area_tag(nil, value, class: :redactor)
+    when :string then text_field_tag(nil, value, class: :'form-control', disabled: true)
+    when :text then text_area_tag(nil, value, rows: 5, class: :'form-control', disabled: true)
+    else column.type == :string ? text_field_tag(nil, value, class: :'form-control', disabled: true) : text_area_tag(nil, value, rows: 5, class: :'form-control', disabled: true)
+    end
+  end
+
   def admin_translations_edit_field f, record, column
     available_models = Translation::AVAILABLE_MODELS[record.class]
     hash = available_models.last.is_a?(Hash) ? available_models.last : {}
     case hash[column.name.to_sym]
     when :redactor then f.text_area(:text, class: :redactor)
-    else column.type == :string ? f.text_field(:text) : f.text_area(:text, class: :'form-control')
+    when :string then f.text_field(:text, class: :'form-control')
+    when :text then f.text_area(:text, rows: 5, class: :'form-control')
+    else column.type == :string ? f.text_field(:text, class: :'form-control') : f.text_area(:text, rows: 5, class: :'form-control')
     end
   end
 
