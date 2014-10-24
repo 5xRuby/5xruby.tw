2f281451cca0cedc579c96d4c64376b418b89530
Tony Jian <tonytonyjan@gmail.com>
2014-10-24 09:39:06 +0800

    locale should be set before set setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :set_seo, :set_locale
+  before_action :set_locale, :set_seo
   helper_method :current_path_with_locale, :translate_record, :tr
 
   def recaptcha?
