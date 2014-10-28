3eca9681899e1f58620c63871818b2df6fc0a7a6
Tony Jian <tonytonyjan@gmail.com>
2014-10-28 17:46:10 +0800

    upgrade rspec, factory_girl

--- spec/factories_spec.rb
+++ spec/factories_spec.rb
@@ -1,9 +1,9 @@
-require 'spec_helper'
+require 'rails_helper'
 
 FactoryGirl.factories.map(&:name).each do |factory_name|
   describe "The #{factory_name} factory" do
     it 'is valid' do
-      expect(build(factory_name)).to be_valid
+      expect(FactoryGirl.build(factory_name)).to be_valid
     end
   end
 end
--- spec/models/concerns/permalinkable_spec.rb
+++ spec/models/concerns/permalinkable_spec.rb
@@ -1,11 +1,8 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe Permalinkable do
-  before :all do
-    class A; include Permalinkable; end
-  end
   it '#next_permalink' do
-    expect(A.next_permalink('rails-123')).to eq 'rails-124'
-    expect(A.next_permalink('rails')).to eq 'rails-1'
+    expect(Permalinkable.next_permalink('rails-123')).to eq 'rails-124'
+    expect(Permalinkable.next_permalink('rails')).to eq 'rails-1'
   end
 end
