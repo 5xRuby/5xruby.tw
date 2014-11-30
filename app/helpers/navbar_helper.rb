ef306538f5b18c993779c514369c7e0f32b5262d
Jian Weihang <tonytonyjan@gmail.com>
2014-12-01 02:17:19 +0800

    acitve the parent item dynamically

--- app/helpers/navbar_helper.rb
+++ app/helpers/navbar_helper.rb
@@ -1,15 +1,20 @@
 module NavbarHelper
   def nav_li text, url, match: url, method: :start_with?, **link_to_options
-    content_tag :li, class: (:active if request.path.send(method, match)) do
+    active = nil
+    @_nav_dropdown_active = active = :active if request.path.send(method, match)
+    # @_nav_dropdown_active is used for nav_dropdown to check active
+    content_tag :li, class: active do
       link_to text, url, **link_to_options
     end
   end
 
   def nav_dropdown text, &block
-    content_tag :li, class: :normal_drop_down do
+    @_nav_dropdown_active = nil
+    items = content_tag(:ul, capture(&block))
+    content_tag :li, class: "normal_drop_down #{@_nav_dropdown_active}" do
       concat link_to text, '#'
       concat content_tag(:div, nil, class: :'mobnav-subarrow')
-      concat content_tag(:ul, capture(&block))
+      concat items
     end
   end
 end
