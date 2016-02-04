76a601df5e403d4dc9ff29e88104447868e4c764
Awaru Ryudo <ryudoawaru@gmail.com>
2016-02-04 17:26:31 +0800

    Upgrade Rails 5.0.0.beta2

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,9 @@ Rails.application.routes.draw do
     post 'rental/calculate'
   end
 
+  get Settings.admin_path_prefix, to: "admin#dashboard", as: :admin_root
   # back
   namespace :admin, path: Settings.admin_path_prefix do
-    root to: :dashboard
     get :space_price
     resources :posts, :courses, :authors, :speakers, :faqs, :categories, :showcases, :videos, :interview_questions do
       put :sort, on: :collection
