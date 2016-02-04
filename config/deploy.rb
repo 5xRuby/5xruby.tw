aa5f58ed26ef111d37c3292472cf97a58343eb75
Awaru Ryudo <ryudoawaru@gmail.com>
2016-02-04 18:42:45 +0800

    Sync capfile lock version with Rails 5

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for Capistrano 3.1
-lock '3.2.1'
+lock '3.4.0'
 
 require 'yaml'
 settings = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))['development']
@@ -20,7 +20,7 @@ set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/upl
 set :default_env, { path: "/usr/local/ruby23/bin:$PATH" }
 
 # Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
 # set :deploy_to, '/var/www/my_app'
