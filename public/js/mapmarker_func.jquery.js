1449e5e5101f8d62c70621db06efe5714b1a8642
Tsehau Chao <jodeci@5xruby.tw>
2016-11-22 14:47:45 +0800

    update google map address

--- public/js/mapmarker_func.jquery.js
+++ public/js/mapmarker_func.jquery.js
@@ -1,13 +1,13 @@
 		//set up markers 
 		var myMarkers = {"markers": [
-				{"latitude": "25.050888", "longitude":"121.516604", "icon": "/img/map-marker2-thumb.png"}
+				{"latitude": "25.042473", "longitude":"121.513803", "icon": "/img/map-marker2-thumb.png"}
 			]
 		};
 		
 		//set up map options
 		$("#map").mapmarker({
 			zoom	: 16,
-			center  : '10349 台灣台北市大同區長安西路 150 號 11 樓之 1',
+			center  : '10046 台灣台北市中正區衡陽路 7 號 5 樓',
 			markers	: myMarkers
 		});
 
