19acd7bd275045b58ce8df1cac85cb7933224c24
PastLeo <chgu82837@gmail.com>
2017-02-17 13:57:11 +0800

    complete news index page #35

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -6,4 +6,13 @@ module PostsHelper
       MarkdownRenderer.render post.content
     end
   end
+
+  POSTS_CATE_TAGS = Settings.posts_cate_tags
+  def posts_cate_tags
+    POSTS_CATE_TAGS
+  end
+
+  def posts_cate_tags_exclude(cate_tags)
+    posts_cate_tags - (cate_tags || [])
+  end
 end
