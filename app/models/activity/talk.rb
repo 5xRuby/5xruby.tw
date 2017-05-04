08aa21f78980e3b06dc8485e257c29acdb9c1cfa
Eric Lee <imdfmy@gmail.com>
2017-05-04 18:21:12 +0800

    add Activity::Camp Activirt::Talk models

---
+++ app/models/activity/talk.rb
@@ -0,0 +1,20 @@
+class Activity::Talk < Activity
+  # scope macros
+
+  # Concerns macros
+
+  # Constants
+
+  # Attributes related macros
+
+  # association macros
+
+  # validation macros
+
+  # callbacks
+
+  # other
+
+  protected
+  # callback methods
+end
