629235e192bc4df431b355a8e51677dbc4a62a26
Eric Lee <imdfmy@gmail.com>
2017-05-05 14:45:33 +0800

    add ActiveCourse model

---
+++ app/models/activity_course.rb
@@ -0,0 +1,21 @@
+class ActivityCourse < ApplicationRecord
+  self.table_name = "activities_courses"
+  # scope macros
+
+  # Concerns macros
+
+  # Constants
+
+  # Attributes related macros
+
+  # association macros
+
+  # validation macros
+
+  # callbacks
+
+  # other
+
+  protected
+  # callback methods
+end
