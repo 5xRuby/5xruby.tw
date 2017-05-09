862925f203d62b2b21affb9d8710c8c5634960d7
Eric Lee <imdfmy@gmail.com>
2017-05-09 13:34:51 +0800

    correct I18n and views

---
+++ app/models/admin/talk.rb
@@ -0,0 +1,20 @@
+class Admin::Talk < Activity::Talk
+  # scope macros
+
+  # Concerns macros
+
+  # Constants
+
+  # Attributes related macros
+
+  # association macros
+
+  # validation macros
+
+  # callbacks
+
+  # other
+
+  protected
+  # callback methods
+end
