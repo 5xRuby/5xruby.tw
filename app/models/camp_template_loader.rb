59d5037c8a4423743ac93883e3cd345b4f00e961
Eric Lee <imdfmy@gmail.com>
2017-05-11 18:45:26 +0800

    remove redundant code in camp_template_loader.rb

--- app/models/camp_template_loader.rb
+++ app/models/camp_template_loader.rb
@@ -1,75 +1,15 @@
-class CampTemplateLoader < Settingslogic
-  source "#{Rails.root}/config/camp.yml"
+class CampTemplateLoader < OpenStruct
+  @@source = YAML.load_file(Rails.root.join('config', 'camp.yml'))
+  cattr_reader :source
 
-  class << self
-    def use_or_default(camp_template)
-      camp_template.nil? ? new : use(camp_template)
-    end
-
-    def use(camp_template)
-      new(camp_template)
-    end
-  end
-
-  def initialize(source = self.class.source, section = nil)
-    case source
-    when nil
-      raise Errno::ENOENT, "No file specified as Settingslogic source"
-    when Hash
-      self.replace source
+  def initialize(template = self.class.source)
+    case template
+    when NilClass
+      raise RuntimeError, "no camp template yaml file given"
     when CampTemplate
-      self.replace source.payload || {}
-      section = "CampTemplate with id = #{source.id}"
-      add_root_methods
+      self.marshal_load(template.payload)
     else
-      file_contents = open(source).read
-      hash = file_contents.empty? ? {} : YAML.load(ERB.new(file_contents).result).to_hash
-      if self.class.namespace
-        hash = hash[self.class.namespace] or return missing_key("Missing setting '#{self.class.namespace}' in #{source}")
-      end
-      self.replace hash
-      add_root_methods
-    end
-    @section = section || self.class.source
-    create_accessors!
-  end
-
-  private
-
-  def add_root_methods
-    extend RootMethods
-  end
-
-  module RootMethods
-    def days_before_begining
-      (begin_date_parsed - Date.today).to_i
-    end
-
-    def begun?
-      @past ||= begin_date_parsed.past?
-    end
-
-    def begin_date_parsed
-      @begin_date_parsed ||= Date.parse(begin_date)
-    end
-
-    def days_before_ending
-      (end_date_parsed - Date.today).to_i
-    end
-
-    def ended?
-      @ended ||= end_date_parsed.past?
-    end
-
-    def end_date_parsed
-      @end_date_parsed ||= Date.parse(end_date)
-    end
-
-    def courses
-      @courses ||= lessons.map do |lesson|
-        raise "Course not found by permalink '#{lesson['course_permalink']}'" unless lesson['record'] = Course.find_by(permalink: lesson['course_permalink'])
-        lesson
-      end
+      self.marshal_load(template)
     end
   end
 end
