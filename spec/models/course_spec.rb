563962eb80de19047f48472188c588ef14b78325
Eric Lee <imdfmy@gmail.com>
2017-05-15 17:28:44 +0800

    factory for ActivityCourse, Activity::Talk, Activity::Camp

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,19 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
+  it { is_expected.to have_many(:stages) }
+  it { is_expected.to have_many(:translations) }
   it { is_expected.to have_and_belong_to_many(:camps) }
   it { is_expected.to have_and_belong_to_many(:talks) }
+  it { is_expected.to have_and_belong_to_many(:speakers) }
 
-  it 'reset_category_counter after_save' do
-    category = create(:category)
-    expect(category.courses_count).to eq 0
-    3.times do
-      course = build(:course)
-      course.category = category
-      course.save!
+  describe "callbacks" do
+    let(:category) { create(:category) }
+    it '#reset_category_counter' do
+      expect(category.courses_count).to eq 0
+      3.times { course = create(:course) }
+      expect(category.reload.courses_count).to eq 3
     end
-    category.reload
-    expect(category.courses_count).to eq 3
   end
-
 end
