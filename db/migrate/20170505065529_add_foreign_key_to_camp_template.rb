d0e2e78501d1c073d27e6eb2c4d6d2decca9d03a
Eric Lee <imdfmy@gmail.com>
2017-05-16 18:10:38 +0800

    remove redudant migrations

--- db/migrate/20170505065529_add_foreign_key_to_camp_template.rb
+++
@@ -1,5 +0,0 @@
-class AddForeignKeyToCampTemplate < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :camp_templates, :activity, foreign_key: true
-  end
-end
--- db/migrate/20170511065747_change_camplate_relation.rb
+++ db/migrate/20170511065747_change_camplate_relation.rb
@@ -1,6 +1,5 @@
 class ChangeCamplateRelation < ActiveRecord::Migration[5.0]
   def change
-    remove_belongs_to :camp_templates, :activity, index: true
     add_reference :activities, :template, foreign_key: { to_table: :camp_templates }
   end
 end
