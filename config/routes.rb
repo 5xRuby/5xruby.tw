62805e9bcfeda12e36e028a7f16f94fadb430d85
Eric Lee <imdfmy@gmail.com>
2017-05-23 18:28:06 +0800

    let admin enable to fill in form_id in admin/activities#new page

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     resources :contacts, only: :create
     resources :showcases, only: :index
     resource :camp, only: :show
+    resource :registration, only: :show
     post 'rental/calculate'
 
     devise_for :users, controllers: { registrations: 'users/registrations' }, skip: :omniauth_callbacks
