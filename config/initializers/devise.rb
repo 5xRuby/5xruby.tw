5aafd6447de443e528280a114cf406f60855b0b5
Eric Lee <imdfmy@gmail.com>
2017-05-31 16:21:55 +0800

    config for action_mailer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,10 +15,10 @@ Devise.setup do |config|
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
+  config.mailer = 'Devise::Mailer'
 
   # Configure the parent class responsible to send e-mails.
-  # config.parent_mailer = 'ActionMailer::Base'
+  config.parent_mailer = 'ActionMailer::Base'
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
