7d4c7b84d92a4212d7932d099f21d6285b2ca34e
Eric Lee <imdfmy@gmail.com>
2017-06-08 12:32:20 +0800

    change drag hanlder

--- app/javascript/packs/admin/activity_form/drag_handle.jsx
+++ app/javascript/packs/admin/activity_form/drag_handle.jsx
@@ -1,4 +1,6 @@
 import {SortableHandle} from 'react-sortable-hoc';
 
-const DragHandle = SortableHandle(() => <span>::</span>);
+const DragHandle = SortableHandle(() =>
+  <span className="glyphicon glyphicon-menu-hamburger"></span>
+  );
 export default DragHandle;
