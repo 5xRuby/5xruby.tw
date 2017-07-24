c7bdbce7f20b525d44db65b301d848d95a4cc687
akiicat <aaaa1379@gmail.com>
2017-07-24 14:52:43 +0800

    update camp img path and banner

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -53,6 +53,10 @@ class Activity < ApplicationRecord
     self.rules.to_json
   end
 
+  def template_type
+    payload['template_type'] || "old" if is_camp?
+  end
+
   protected
   # callback methods
   def touch_if_needed(course)
