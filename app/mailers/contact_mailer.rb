5ac8f1adb6479ad9e685a6b361b380546f014b8c
Awaru Ryudo <ryudoawaru@gmail.com>
2017-08-16 18:46:02 +0800

    Fix mailer issue

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ActionMailer::Base
   # default from: "from@example.com"
   def enquire contact
     @contact = contact
-    mail to: Settings.contact_email, from: contact.email
+    mail to: Settings.contact_email, from: contact.email, sender: Settings.action_mailer.default_options.from
   end
 end
