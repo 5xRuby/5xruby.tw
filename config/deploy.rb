67e95e98735e93d10d3d27e68a6e18942e053e51
蒼時弦也 <elct9620@frost.tw>
2018-03-08 18:39:52 +0800

    Update deploy config to Digital Ocean

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :repo_url, 'git@github.com:5xRuby/5xruby.tw.git'
 set :branch, 'master'
 set :bundle_binstubs, false
 
-set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads}
 
 
