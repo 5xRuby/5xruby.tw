8e20bee8020f453d196c93ae7d2125477808c20a
Awaru Ryudo <ryudoawaru@gmail.com>
2018-07-06 11:46:13 +0800

    Use Hash instead of ERB to render yaml part

--- app/models/concerns/middleman_post_renderer.rb
+++ app/models/concerns/middleman_post_renderer.rb
@@ -2,21 +2,27 @@ module MiddlemanPostRenderer
   extend ActiveSupport::Concern
 
   Template = ERB.new <<-EOF
----
-
-title: <%=title%>
-date: <%=publish_at.utc.strftime("%Y-%m-%d %h:%m %Z")%>
-slug: <%=permalink%>
-author: <%=author.try(:name)%>
-tags: <%=tag_list.join ", "%>
-category: <%=main_cat%>
-cover_image: <%=image.to_s.gsub("/uploads/","")%>
-markup_type: <%=markup_type%>
-
+<%=mman_meta_data.to_yaml%>
 ---
 
 <%=content%>
 EOF
+
+  def mman_meta_data
+    {
+      title: title,
+      date: publish_at.utc.strftime("%Y-%m-%d %H:%M %Z"),
+      slug: permalink,
+      author: author.try(:name),
+      tags: tag_list.join(', '),
+      category: main_cat,
+      image: {
+        original: image.to_s.gsub("/uploads/", ''),
+        preview: image.preview.to_s.gsub("/uploads/", ''),
+        thumb: image.thumb.to_s.gsub("/uploads/", '')
+      }
+    }
+  end
 
   def output_file_name
     mfn = [publish_at.utc.strftime('%Y-%m-%d'), permalink].join("-")
@@ -28,7 +34,7 @@ EOF
   end
 
   def main_cat
-    Settings.posts_cate_tags & tag_list
+    (Settings.posts_cate_tags & tag_list).first.to_s
   end
 
   def as_middleman_post
